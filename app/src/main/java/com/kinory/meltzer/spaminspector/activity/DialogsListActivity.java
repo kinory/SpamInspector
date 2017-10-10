8d4e10cb26c8e332f46ed2f71c0974b854a81837
Gilad Kinory <giladkinory2000@gmail.com>
2017-10-10 14:05:00 +0300

    Added request permission

--- app/src/main/java/com/kinory/meltzer/spaminspector/activity/DialogsListActivity.java
+++ app/src/main/java/com/kinory/meltzer/spaminspector/activity/DialogsListActivity.java
@@ -1,9 +1,15 @@
 package com.kinory.meltzer.spaminspector.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.kinory.meltzer.spaminspector.R;
 import com.kinory.meltzer.spaminspector.model.Dialog;
@@ -15,12 +21,21 @@ import com.stfalcon.chatkit.dialogs.DialogsListAdapter;
 
 public class DialogsListActivity extends AppCompatActivity {
 
+    private static final int READ_SMS_PERMISSIONS_REQUEST = 1;
+
     private DialogsListAdapter<Dialog> dialogsListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialogs_list);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            getPermissionToReadSMS();
+        } else {
+            refreshSmsInbox();
+        }
 
         DialogsList dialogsList = (DialogsList) findViewById(R.id.dialogsList);
         dialogsListAdapter = new DialogsListAdapter<>(null);
@@ -37,4 +52,42 @@ public class DialogsListActivity extends AppCompatActivity {
         });
 
     }
+
+    /**
+     * Gets the needed permissions to read sms.
+     */
+    public void getPermissionToReadSMS() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_SMS)) {
+                Toast.makeText(this, "Please allow permission!", Toast.LENGTH_SHORT).show();
+            }
+            requestPermissions(new String[]{Manifest.permission.READ_SMS}, READ_SMS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_SMS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read SMS permission granted", Toast.LENGTH_SHORT).show();
+                refreshSmsInbox();
+            } else {
+                Toast.makeText(this, "Read SMS permission denied", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void refreshSmsInbox() {
+        // TODO: write some code in here
+    }
+
 }
